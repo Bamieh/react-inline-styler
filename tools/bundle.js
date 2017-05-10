ff2842ee0a1903ad32479512fdb25055497f7f20
Bamieh <ahmadbamieh@gmail.com>
2017-05-10 11:10:11 +0300

    update build process

--- tools/bundle.js
+++ tools/bundle.js
@@ -4,10 +4,17 @@ import { makeDir } from './lib/fs';
 async function bundle() {
   await makeDir('distribution');
   await new Promise((resolve, reject) => {
-     // cp.spawnSync('yarn', ['build', '-t', pkg.name, '.'], { stdio: 'inherit' });
-     // babel src --out-file distribution/index.js
-     cp.spawnSync('babel', ['src', '--out-file', 'distribution/index.js'], { stdio: 'inherit' });
-     resolve();
+    // cp.spawnSync('yarn', ['build', '-t', pkg.name, '.'], { stdio: 'inherit' });
+    // babel src --out-file distribution/index.js
+    cp.spawnSync('yarn', [
+      'build:babel'
+      // 'src',
+      // '--compact=true',
+      // '--no-comments',
+      // '--out-file=distribution/index.js',
+    ], { stdio: 'inherit' });
+
+    resolve();
   });
 }
 
